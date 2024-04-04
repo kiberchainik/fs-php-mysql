9d7a200ee00cf5b74956520acbd3f12c87b244fb
Alexandr <ya.sacher@gmail.com>
2024-04-04 23:53:10 +0200

    Update DBConfig.php

--- Core/Config/DB/DBConfig.php
+++ Core/Config/DB/DBConfig.php
@@ -1,11 +1,11 @@
 <?php
    return array(
-        'dbname' => 'urlpyrtf_fs',
-        'host' => 'localhost',
-        'port' => '3306',
-        'user' => 'urlpyrtf_fs',
-        'pass' => '62789242aLEX!',
-        'charset' => 'utf8',
-        'db_pref' => 'tori_'
+        'dbname' => '***',
+        'host' => '***',
+        'port' => '***',
+        'user' => '***',
+        'pass' => '***',
+        'charset' => '***',
+        'db_pref' => '***'
    );
 ?>
